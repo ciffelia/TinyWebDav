36855559729ecf2f8b6d611f24e2aec7130c380f
Andrew Leech <andrew@alelec.net>
2016-06-24 09:15:05 +1000

    Add setuptools script for easy install.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+version = '1.0'
+
+setup(
+    name='TinyWebDav',
+    version=version,
+    author='Lai ChuJiang',
+    py_modules = ['webdav'],
+    url='https://github.com/andrewleech/TinyWebDav',
+    download_url='https://github.com/andrewleech/TinyWebDav/archive/gh-pages.zip',
+    description='A Single File,Tiny, Python WebDav Server. Original package from https://github.com/wolf71/TinyWebDav',
+)
